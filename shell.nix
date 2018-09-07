7fbf830a8a66f345837adbcb93ff6500813a02ee
techtangents <dylan@techtangents.com>
2018-09-07 15:34:19 +1000

    nix and gitignore

---
+++ shell.nix
@@ -0,0 +1,12 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc861" }:
+let
+  inherit (nixpkgs) pkgs;
+  ghc = pkgs.haskell.packages.${compiler}.ghcWithPackages (ps: with ps; [
+          mtl
+        ]);
+in
+pkgs.stdenv.mkDerivation {
+  name = "kinds-talk";
+  buildInputs = [ ghc ];
+  shellHook = "eval $(egrep ^export ${ghc}/bin/ghc)";
+}
